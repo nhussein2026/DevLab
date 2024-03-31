1b4530763fdfecf3360491fbe8f9f91aec07be05
nhussein2026 <naser738nas@gmail.com>
2024-03-31 14:47:07 +0300

    edit the code to run

--- assets/js/viewerCounter.js
+++ assets/js/viewerCounter.js
@@ -1,5 +1,17 @@
-fetch('visitor-count.json')
-.then(response => response.json())
-.then(data => {
-  document.getElementById('visitorCount').textContent = data.count;
-});
+// fetch('visitor-count.json')
+// .then(response => response.json())
+// .then(data => {
+//   document.getElementById('visitorCount').textContent = data.count;
+// });
+
+const visitorCountElement = document.getElementById('visitorCount');
+
+fetch('assets\json\visitor-count.json')
+    .then(response => response.json())
+    .then(data => {
+        visitorCountElement.textContent = data.count;
+    })
+    .catch(error => {
+        console.error('Error fetching visitor count:', error);
+        // Optionally display an error message to the user
+    });
